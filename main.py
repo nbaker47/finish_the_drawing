6714968710667ce503cfb6e10e7d3d83f38b10b6
nbaker47 <nathan.baker.2647@gmail.com>
2023-07-06 21:12:14 -0400

    css

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask, jsonify, render_template, request, send_from_directory, redirect, url_for, g
+from flask import Flask, jsonify, render_template, request, send_from_directory, redirect, url_for, g, Response
 import nltk
 from nltk.corpus import wordnet
 from datetime import date, datetime, timedelta
 import random
 import base64
 import sqlite3
+from google.cloud import storage
+from PIL import Image
+import io
+from io import BytesIO
+
 
 # Dictionary to store the vote scores for each image
 image_votes = {}
@@ -55,38 +60,113 @@ def before_request():
     g.sqlite_db = sqlite3.connect('app.db')
     g.cursor = g.sqlite_db.cursor()
 
+
 # Handle the /submit route
 @app.route('/submit', methods=['POST'])
 def submit():
+    
+    print('submitting image')
+    
     # Get the image data from the POST request
-    image_data = request.form['image']
+    base64_image = request.form['image']
+    
+    print('read image')
+    
+    # Extract the encoded image data from the base64 string
+    encoded_image = base64_image.split(',')[1]
 
-    # Store the image data in the database
-    g.cursor.execute('INSERT INTO images (image_data) VALUES (?)', (image_data,))
-    g.sqlite_db.commit()
+    # Decode the base64 image data
+    decoded_image = base64.b64decode(encoded_image)
 
-    # Get the last inserted row id
-    image_id = g.cursor.lastrowid
+    # Create a BytesIO object to work with the binary image data
+    image_io = BytesIO(decoded_image)
+
+    # Open the image from the BytesIO object
+    image = Image.open(image_io)
+    
+    #----------------------
+    
+    image_io = BytesIO()
+    
+    image.save(image_io, format='PNG')
+    
+    # Reset the stream position to the beginning
+    image_io.seek(0)
+
+    # Set up GCS client
+    storage_client = storage.Client()
+
+    # Retrieve the bucket
+    bucket = storage_client.bucket('drawoff-391919.appspot.com')
+
+    # Generate a unique filename
+    filename = f"image_{datetime.now().strftime('%Y%m%d%H%M%S')}.png"
+
+    # Upload the image data to the bucket
+    blob = bucket.blob(filename)
+    blob.upload_from_file(image_io, content_type='image/png')
 
     # Redirect to the /view route
     return redirect(url_for('view'))
 
+
+# Handler for the route that deletes .png files
+@app.route('/delete-png-files', methods=['POST'])
+def delete_png_files():
+    # Set up GCS client
+    storage_client = storage.Client()
+    bucket_name = 'drawoff-391919.appspot.com'
+
+    # Retrieve the bucket
+    bucket = storage_client.bucket(bucket_name)
+
+    # List all blobs in the bucket
+    blobs = bucket.list_blobs()
+
+    # Iterate through the blobs and delete .png files
+    for blob in blobs:
+        if blob.name.endswith('.png'):
+            blob.delete()
+
+    return 'Deletion of .png files completed'
+
+
+
 # Handle the /view route
 @app.route('/view')
 def view():
-    # Retrieve all image data from the database
-    g.cursor.execute('SELECT image_data FROM images')
-    image_data_rows = g.cursor.fetchall()
+    # Set up GCS client
+    storage_client = storage.Client()
 
-    # Extract image data from the rows
-    image_datas = [row[0] for row in image_data_rows]
+    # Retrieve the bucket
+    bucket = storage_client.bucket('drawoff-391919.appspot.com')
+
+    # List all blobs in the bucket
+    blobs = bucket.list_blobs()
+
+    # Get the image filenames from the blobs
+    image_filenames = [blob.name for blob in blobs if blob.name.endswith('.png')]
     
-    print("Image Data:", image_datas)
+    print(image_filenames)
+    print('TRYING TO serving image')
 
-    return render_template('submissions.html', submissions=image_datas, vote_count=vote_count)
+    urls = []
+
+    for image_filename in image_filenames:
+
+        # Get the publicly accessible URL of the uploaded image
+        blob = bucket.blob(image_filename)
+        image_url = blob.public_url
+        
+        urls.append(image_url)
+
+    return render_template('submissions.html', submissions=urls, vote_count=vote_count)
+
+
 
 # entry point
 if __name__ == '__main__':
     # Route to serve the favicon1.ico file
-    app.add_url_rule('/favicon.ico', redirect_to=url_for('static', filename='favicon.ico'))
+    # g.cursor.execute("DELETE FROM images")
+    app.add_url_rule('/favicon.ico', redirect_to=url_for('static', filename='favicon1.ico'))
     app.run(debug=False)
